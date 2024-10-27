12e22ab50f42926e8b99b57db9088c3cd27305bb
drpafowler <drpafowler@gmail.com>
2024-10-27 10:15:59 -0700

    added switch to filter data

--- app.py
+++ app.py
@@ -12,17 +12,11 @@ from shinywidgets import render_widget
 ui.page_opts(title=ui.h1("Philip's Penguins", style="text-align: center;"), fillable=True)
 
 
-with ui.sidebar(title=ui.h2("Seaborn Controls")):
-    ui.input_select("plot", "Plot Type", ["Scatterplot", "Histogram"])
-    ui.input_select("xaxis", "X-axis", ["bill_length_mm", "bill_depth_mm", "body_mass_g"], selected="bill_length_mm")
-    ui.input_select("yaxis", "Scatterplot Y-axis", ["bill_length_mm", "bill_depth_mm", "body_mass_g"], selected="bill_depth_mm")
-    ui.input_slider("bins", "Number of bins", 5, 50, 20)
+with ui.sidebar(title=ui.h2("Global Controls"), width="400px"):
+    ui.input_select("xaxis", "X-axis (all plots)", ["bill_length_mm", "bill_depth_mm", "body_mass_g"], selected="bill_length_mm")
+    ui.h4("Filter Controls")
+    ui.input_switch("filter", "Filter Data", True)
     ui.input_slider("mass", "Mass", 2000, 6000, [2000,6000])
-
-    ui.h3("Plotly Controls")
-    ui.input_numeric("plotly_bins", "Number of bins", 20, min=5, max=50)
-
-    ui.h3("Filters")
     ui.input_checkbox_group(
         "sex",
         "Sex",
@@ -40,7 +34,17 @@ with ui.sidebar(title=ui.h2("Seaborn Controls")):
         "Island",
         ["Biscoe", "Dream", "Torgersen"],
         selected=["Biscoe", "Dream", "Torgersen"],
-    )
+    )   
+    ui.hr()
+    ui.h4("Seaborn Controls")
+    ui.input_select("plot", "Plot Type", ["Scatterplot", "Histogram"])
+    ui.input_select("yaxis", "Scatterplot Y-axis", ["bill_length_mm", "bill_depth_mm", "body_mass_g"], selected="bill_depth_mm")
+    ui.input_slider("bins", "Number of bins", 5, 50, 20)
+
+    ui.hr()
+    ui.h4("Plotly Controls")
+    ui.input_numeric("plotly_bins", "Number of bins", 20, min=5, max=50)
+
     ui.a("GitHub", href="https://github.com/drpafowler/cintel-02-data", target="_blank")
 
 
@@ -81,20 +85,34 @@ with ui.layout_columns():
         @render.plot
         def length_depth():
             if input.plot() == "Scatterplot":
-                return sns.scatterplot(
-                    data=filtered_df(),
-                    x=input.xaxis(),
-                    y=input.yaxis(),
-                    hue="species",
-                )
+                if input.filter():
+                    return sns.scatterplot(
+                        data=filtered_df(),
+                        x=input.xaxis(),
+                        y=input.yaxis(),
+                        hue="species",
+                    )
+                else:
+                    return sns.scatterplot(
+                        data=filtered_df(),
+                        x=input.xaxis(),
+                        y=input.yaxis(),
+                    )
             elif input.plot() == "Histogram":
-                return sns.histplot(
-                    data=filtered_df(),
-                    x=input.xaxis(),
-                    bins=input.bins(),
-                    hue="species",
-                    multiple="stack",
-                )
+                if input.filter():
+                    return sns.histplot(
+                        data=filtered_df(),
+                        x=input.xaxis(),
+                        bins=input.bins(),
+                        hue="species",
+                        multiple="stack",
+                    )
+                else:
+                    return sns.histplot(
+                        data=filtered_df(),
+                        x=input.xaxis(),
+                        bins=input.bins(),
+                    )
 
     with ui.card(full_screen=True):
         ui.card_header("Penguin data")
@@ -117,14 +135,23 @@ with ui.layout_columns():
 
         @render_widget
         def plotly_plot():
-            fig = px.histogram(
-            filtered_df(),
-            x=input.xaxis(),
-            color="species",
-            marginal="box",
-            title="Histogram of Penguin Data",
-            nbins=input.plotly_bins()
-            )
+            if input.filter():
+                fig = px.histogram(
+                    filtered_df(),
+                    x=input.xaxis(),
+                    color="species",
+                    marginal="box",
+                    title="Histogram of Penguin Data",
+                    nbins=input.plotly_bins()
+                )
+            else:
+                fig = px.histogram(
+                    filtered_df(),
+                    x=input.xaxis(),
+                    marginal="box",
+                    title="Histogram of Penguin Data",
+                    nbins=input.plotly_bins()
+                )
             return fig
 
         
@@ -134,6 +161,8 @@ ui.include_css(app_dir / "styles.css")
 
 @reactive.calc
 def filtered_df():
+    if not input.filter():
+        return df
     filt_df = df[df["species"].isin(input.species())]
     filt_df = filt_df[filt_df["sex"].isin(input.sex())]
     filt_df = filt_df[filt_df["island"].isin(input.island())]
